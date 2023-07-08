ae91881b194f556fb62462b9d689bb850aa0500e
Isabelli D Oliveira <isabellioliveira821@gmail.com>
2023-07-08 20:57:08 -0300

    questão 5 resolvida

--- manipulando_string3.py
+++ manipulando_string3.py
@@ -7,20 +7,4 @@ a) Input: "badab"
 b) Output: "Bab"(Observação: Pode haver várias saídas validas, você
 só precisa retornar uma delas.)'''
 
-def encontrar_substring_palindroma():
-    string = str(input('Digite uma frase: '))
-
-    substring_palindroma_mais_longa = ""
-
-    for i in range(len(string)):
-        for j in range(i+1, len(string)+1):
-            substring = string[i:j]
-            if substring == substring[::-1] and len(substring) > len(substring_palindroma_mais_longa):
-                substring_palindroma_mais_longa = substring
-
-    return substring_palindroma_mais_longa
-substring_palindroma = encontrar_substring_palindroma()
-print(substring_palindroma)
-
-
-print1 = encontrar_substring_palindroma
+frase = input()
--- manipulando_string5.py
+++ manipulando_string5.py
@@ -1,2 +1,24 @@
 # Verifique se a string é um anagrama de um palindromo. 
-frase = str(input('Digite uma frase: '))
+
+def anagrama_de_um_palindromo(palavra):
+    palavra = palavra.lower()
+    # Verifica se é um palíndromo
+    for i in range(len(palavra)):
+        palindromo = False 
+        repete = False 
+        if palavra[i] == palavra[-(i+1)]:
+            repete = True
+            palindromo = True 
+
+    if palindromo == True:
+        return True
+    else:
+        return False
+
+
+teste1 = anagrama_de_um_palindromo('Radar')
+print(teste1)
+teste2 = anagrama_de_um_palindromo('Ovo')
+print(teste2)
+teste3 = anagrama_de_um_palindromo('Frango')
+print(teste3)
